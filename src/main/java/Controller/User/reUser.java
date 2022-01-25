7876f0df27577e0dce62cff5084b5e93785447b5
devBarahona <43079957+FranBarahona@users.noreply.github.com>
2022-01-25 12:26:40 -0600

    Update reUser.java

--- src/main/java/Controller/User/reUser.java
+++ src/main/java/Controller/User/reUser.java
@@ -1,39 +1,33 @@
-package UI;
+package Controller.User;
+
 import Model.UserDetails;
-import Model.UserModel.User;
 
 import java.util.Scanner;
-import static Controller.User.reUser.reUsers;
-import static UI.UIMenu.showMenu;
 
-public class UIResgisterUser {
-    public static void registerUser(){
-        UserDetails user = new User();
-        int response = 0;
-        do{
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Write your name");
-            user.setName(sc.nextLine());
-            System.out.println("Write your lastName");
-            user.setLastName(sc.nextLine());
-            System.out.println("Write your nickname");
-            user.setNickname(sc.nextLine());
-            System.out.println("Write your password");
-            user.setPassword(sc.nextLine());
+import static Controller.User.CrudUser.*;
+import static Model.UserModel.Users.userList;
+import static UI.UIResgisterUser.registerUser;
 
-            System.out.println("1.Confirm");
-            System.out.println("2.back");
-            System.out.println("3.exit");
-            response = Integer.parseInt(sc.nextLine());
-            switch (response){
-                case 1 ->{
-                    reUsers(user);
-                    response = 0;
-                }
-                case 2 -> registerUser();
-                case 3 -> response = 0;
-                default -> System.out.println("Option invalid");
+public class reUser{
+
+    public static void reUsers(UserDetails user){
+        Scanner sc = new Scanner(System.in);
+        try{
+            //Recupera usuarios actuales y add nuevos
+            CrudUser cUser = new CrudUser();
+            cUser.read();
+            //Registra nuevos usuarios
+            if(userList.containsKey(user.getNickname())){
+                System.out.println("this name is not available");
+                System.out.println("please write an available nickname");
+                sc.nextLine();
+                registerUser();
+            }else{
+                cUser.setUser(user);
+                cUser.write();
             }
-        }while(response != 0);
+        } catch(Exception e){
+            System.err.println(e.getMessage());
+        }
     }
 }
